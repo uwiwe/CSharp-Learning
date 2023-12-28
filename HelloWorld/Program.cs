d107d8958162a04bbdcd9693f8bf1a9ee0e5e82d
wiwe <wiweurdaneta@gmail.com>
2023-12-28 16:37:27 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+            for (int d = 0; d < 5; d++)
+            { 
+                Console.WriteLine("Yes"); 
+            }
+
+        }
+    }
+}
